858bdbf1f95d2fd0f5273d22263f58b717deaeaf
Heeyoung Joe <developers.ahead@gmail.com>
2025-09-03 20:09:44 +0900

    250903 5 min

---
+++ part1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+/*001 Starting C++ Programing*/
+int p_001(){
+    cout << "Hello C++ World!" <<endl;
+
+    return 0;
+}
+
+/*002 Output on console*/
+int p_002(){
+    int number = 0;
+    cin >> number;
+    cout << "input number is" << number << "!" << endl;
+
+    return 0;
+}
+int main(){
+    //p_001();
+    // p_002();66
+}
+
