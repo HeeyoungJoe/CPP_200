6ec9b1649c1c248dce66234ca637ade5ddfcdc51
Heeyoung Joe <developers.ahead@gmail.com>
2025-09-15 07:55:10 +0900

    270915 little

--- part2_25-33.cpp
+++ part2_25-33.cpp
@@ -35,10 +35,28 @@ void p26(){
     return;
 }
 void p27(){
+    int goguryeo = 37, baekjae = 18, silla = 57;
 
+    printf("삼국 건국연도\n");
+    printf("고구려 bc%d년, 백제 bc%d년, 신라 bc%d년\n",goguryeo, baekjae, silla);
 }
 void p28(){
+    bitset<8> bit1;
+    bit1.reset();
+    bit1=127;//0111 1111
 
+    bitset<8> bit2;
+    bit2.reset();
+    bit2=0x20;//32
+
+    bitset<8> bit3 = bit1 & bit2;
+    bitset<8> bit4 = bit1 | bit2;
+    bitset<8> bit5 = bit1 ^ bit2;
+    bitset<8> bit6 = ~bit1;
+    bitset<8> bit7 = bit2 << 1;
+    bitset<8> bit8 = bit2 >> 1;
+
+    
 }
 void p29(){
 
@@ -58,8 +76,8 @@ void p33(){
 
 int main(){
     //p25();
-    p26();
-    //p27();
+    //p26();
+    p27();
     //p28();
     //p29();
     //p30();
