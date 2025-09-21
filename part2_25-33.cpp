0b198d511ccb8627c95723ca20315c2169b0ffd7
Heeyoung Joe <developers.ahead@gmail.com>
2025-09-21 23:46:08 +0900

    270915 little

--- part2_25-33.cpp
+++ part2_25-33.cpp
@@ -56,16 +56,50 @@ void p28(){
     bitset<8> bit7 = bit2 << 1;
     bitset<8> bit8 = bit2 >> 1;
 
-    
+    cout<<"bit1&bit2:"<<bit3<<","<<bit3.to_ulong()<<endl;
+    cout<<"bit1|bit2:"<<bit4<<","<<bit4.to_ulong()<<endl;
+    cout<<"bit1^bit2:"<<bit5<<","<<bit5.to_ulong()<<endl;
+    cout<<"~bit1:"<<bit6<<","<<bit6.to_ulong()<<endl;
+    cout<<"bit2<<1:"<<bit7<<","<<bit7.to_ulong()<<endl;
+    cout<<"bit2>>1:"<<bit8<<","<<bit8.to_ulong()<<endl;   
 }
 void p29(){
+    int x = 2;
+    double y = 4.4;
 
+    int i = static_cast<int>(y/x);
+    int j = (int)y/x;
+    double k = y/x;
+
+    cout <<"4.4 / 2 = (static_cast<int>)"<<i<<endl;
+    cout <<"4.4 / 2 = (int)" << j <<endl;
+    cout <<"4.4 / 2 = "<<k<<endl;
 }
 void p30(){
 
-}
-void p31(){
+    int number1=65;
+    double number2=23.4;
 
+    int number3=int(number2);
+    double number4=double(number1/number2);
+
+    char ch = char(number1);
+
+    cout<<"number1:"<<number1<<endl;
+    cout<<"number2:"<<number2<<endl;
+    cout<<"number3:"<<number3<<endl;
+    cout<<"number4:"<<number4<<endl;
+    cout<<"ch:"<<ch<<endl;
+}
+class Temp{
+    int no;
+    bool is_on;
+};
+void p31(){
+    cout<<"char 크기:"<<sizeof('p')<<endl;
+    cout<<"int 크기:"<<sizeof(10)<<endl;
+    cout<<"double 크기:"<<sizeof(10.0)<<endl;
+    cout<<"class 크기:"<<sizeof(Temp)<<endl;
 }
 void p32(){
 
@@ -77,11 +111,11 @@ void p33(){
 int main(){
     //p25();
     //p26();
-    p27();
+    //p27();
     //p28();
     //p29();
-    //p30();
-    //p31();
+    p30();
+    p31();
     //p32();
     //p33();
     return 0;
