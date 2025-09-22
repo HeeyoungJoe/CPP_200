26631e23cf3ec05bdd2f16a19f1df38062ea5180
Heeyoung Joe <developers.ahead@gmail.com>
2025-09-22 22:30:29 +0900

    270922 dumped today2

--- part2_25-33.cpp
+++ part2_25-33.cpp
@@ -102,6 +102,39 @@ void p31(){
     cout<<"class 크기:"<<sizeof(Temp)<<endl;
 }
 void p32(){
+    int number1 = 100;
+    int number2 = 6;
+
+    if(number1<number2)
+        cout<<"number2가 더 큽니다."<<endl;
+    else if(number1>number2)
+        cout<<"number1이 더 큽니다."<<endl;
+    else   
+        cout<<"number1과 number2는 같습니다"<<endl;
+
+    if(number2<7)
+    {
+        if(number2>3){
+            if(number2 ==4)
+                cout <<"number2는 4입니다"<<endl;
+            else if(number2==5)
+                cout<<"number2는 5입니다"<<endl;
+            else
+                cout<<"number2는 6입니다"<<endl;
+        }
+        else{
+            if(number2 == 3){
+                cout<<"number2는 3입니다"<<endl;
+            }
+            else if(number2 ==2)
+                cout<<"number2는 2입니다"<<endl;
+            else    
+                cout<<"number2는 1이하 입니다"<<endl;
+        }
+    }
+
+}
+void p33(){
     int number1= 100;
     int number2= 200;
     int number3= 300;
@@ -114,9 +147,6 @@ void p32(){
         cout<<"가장 큰 수 number3:"<<number3<<endl;
     else
         cout<<"찾을 수 없습니다."<<endl;
-}
-void p33(){
-
 }
 
 int main(){
